1ed392891e1e8d69d7406c45ef91fec98a329f1b
Mohamed Sayed <ms10596@gmail.com>
2019-05-04 15:09:40 +0200

    feat: tools

--- src/js/components/Tools.js
+++ src/js/components/Tools.js
@@ -3,15 +3,39 @@ const e = React.createElement;
 class Tools extends React.Component {
     render() {
         return (<div className="card mt-3">
-                <div className="card-title card-header"><i className="fas fa-code"/> Tools</div>
-                <ul className="list-group">
-                    <li className="list-group-item">Python (Tensorflow, Keras, Numpy, Matplotlib,
-                        Flask)
-                    </li>
-                    <li className="list-group-item">Linux</li>
-                    <li className="list-group-item">SQL</li>
-                </ul>
-            </div>)
+            <div className="card-title card-header"><i className="fas fa-book"/> Tools</div>
+            <table className="table table-bordered">
+
+                <tbody>
+                <tr>
+                    <td rowSpan={4}>Web</td>
+                    <td>Flask</td>
+                </tr>
+                <tr>
+                    <td>PostgreSQL</td>
+                </tr>
+                <tr>
+                    <td>React</td>
+                </tr>
+                <tr>
+                    <td>Bootstrap</td>
+                </tr>
+                <tr>
+                    <td rowSpan={4}>Data</td>
+                    <td>Numpy</td>
+                </tr>
+                <tr>
+                    <td>Keras</td>
+                </tr>
+                <tr>
+                    <td>Pandas</td>
+                </tr>
+                <tr>
+                    <td>Scikit-learn</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>)
     }
 }
 

038a21c7696fb5cbd4f2e1fa29a62cc0ec66d020
Mohamed Sayed <ms10596@gmail.com>
2019-05-05 22:46:27 +0200

    feat: adding projects

--- src/js/components/Projects.js
+++ src/js/components/Projects.js
@@ -3,19 +3,20 @@ const e = React.createElement;
 class Projects extends React.Component {
     render() {
         return (<div className="card mt-3">
-            <div className="card-title card-header"><i className="fas fa-tasks"/> Projects</div>
-            <ul className="list-group">
-                <a className="list-group-item" href="https://github.com/ms10596/ngrams">Autocomplete
-                    Search
-                    Query</a>
-                <a className="list-group-item" href="https://github.com/ms10596/detectectioner">Face
-                    Recognition Application</a>
-                <a className="list-group-item" href="https://github.com/ms10596/Musicaly">Music
-                    Player</a>
-                <a className="list-group-item" href="https://github.com/ms10596/catalog">TV Series
-                    Managment
-                    System</a>
-            </ul>
+            <div className="card-title card-header"><i className="fas fa-tasks"/> Recent Projects</div>
+            <table className="table table-bordered">
+                <tbody>
+                <tr>
+                    <td><a href="https://github.com/ms10596/match" target="_blank">match:</a> My graduation project is based on a popular natural language processing problem which is Text Readability Assessment. We aim in this project to classify any given text to one of 3 levels Easy, Intermediate and hard. We are using OneStopEnglish corpus to build our deep learning model. </td>
+                </tr>
+                <tr>
+                    <td><a href="https://github.com/ms10596/catalog" target="_blank">catalog:</a> TV series management system using <b>Python</b>-Flask as a micro framework, SqlAlchemy as a operational relational mapping and Amazon Web Services for deployment. </td>
+                </tr>
+                <tr>
+                    <td><a href="https://github.com/ms10596/ms10596.github.io" target="_blank">My personal resume:</a> I have built this website using React which is a frontend <b>javascript</b> framework, <b>HTML</b> and <b>Bootstrap</b>. I have deployed this website on github hosting service.</td>
+                </tr>
+                </tbody>
+            </table>
         </div>)
     }
 }

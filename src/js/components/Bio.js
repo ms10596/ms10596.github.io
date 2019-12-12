d664b0f7f2393391834f680f0712ac4d6b340d24
ms10596 <ms10596@gmail.com>
2019-12-12 15:05:23 +0200

    typo

--- src/js/components/Bio.js
+++ src/js/components/Bio.js
@@ -3,7 +3,7 @@ class Bio extends React.Component {
         return (<div className="card mt-3">
             <div className="card-body">
                 <p className="card-text">
-                    Bringing ideas into real world through developing with python. Had some experience in data science especially in nlp.
+                    Bringing ideas into real world through developing with python. Had some experience in data science and nlp.
                 </p>
             </div>
         </div>);

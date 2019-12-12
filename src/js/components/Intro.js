f3cd3ac71c9f8bf61dffd4fefb425c7db7974e5d
Mohamed Sayed <ms10596@gmail.com>
2019-12-12 14:28:42 +0200

    mobile number

--- src/js/components/Intro.js
+++ src/js/components/Intro.js
@@ -5,30 +5,30 @@ class Intro extends React.Component {
             <h4 className="font-italic font-weight-light text-center mt-2">Computer Scientist</h4>
             <table className="table table-bordered mt-1">
                 <tbody>
-                <tr>
-                    <td colSpan={2}><i className="fas fa-phone fa-lg"/> +20123456789</td>
-                </tr>
-                <tr>
-                    <td colSpan={2}><i className="fas fa-home fa-lg"/> 2XMM+83 Sheikh Zayed City
+                    <tr>
+                        <td colSpan={2}><i className="fas fa-phone fa-lg" /> +201146613779</td>
+                    </tr>
+                    <tr>
+                        <td colSpan={2}><i className="fas fa-home fa-lg" /> 2XMM+83 Sheikh Zayed City
                     </td>
-                </tr>
-                <tr>
-                    <td colSpan={2}><i className="fas fa-envelope fa-lg"/> ms10596@gmail.com</td>
-                </tr>
+                    </tr>
+                    <tr>
+                        <td colSpan={2}><i className="fas fa-envelope fa-lg" /> ms10596@gmail.com</td>
+                    </tr>
                 </tbody>
             </table>
-            <img className="card-img-bottom" src="./src/img/svg/me.jpg" alt="me"/>
+            <img className="card-img-bottom" src="./src/img/svg/me.jpg" alt="me" />
             {/*<img src="./src/img/svg/me2.jpg" alt="me"/>*/}
-            
-            <div className="mt-1"/>
+
+            <div className="mt-1" />
 
             <div className="btn-group btn-group-lg">
                 <a className="btn btn-outline-dark" href="https://www.github.com/ms10596" target="_blank"
-                   style={{width: "50%"}}><i className="fab fa-github fa-lg"/></a>
+                    style={{ width: "50%" }}><i className="fab fa-github fa-lg" /></a>
                 <a className="btn btn-outline-dark" href="https://www.linkedin.com/in/ms10596" target="_blank"
-                   style={{width: "50%"}}><i className="fab fa-linkedin fa-lg"/></a>
+                    style={{ width: "50%" }}><i className="fab fa-linkedin fa-lg" /></a>
             </div>
-            <div className="mt-1"/>
+            <div className="mt-1" />
         </div>)
     }
 }

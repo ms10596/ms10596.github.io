08314ce07502b06d927c0fbcd10e8001e02b1ba0
ms10596 <ms10596@gmail.com>
2020-03-07 16:42:06 +0200

    refactor: moving data to states

--- src/js/components/Certificates.js
+++ src/js/components/Certificates.js
@@ -1,43 +1,59 @@
 class Certificates extends React.Component {
+    state = {
+        certificates: [
+            {
+                name: "Cloud Developer for Enterprise Nanodegree",
+                link: "https://confirm.udacity.com/6YYLUWEA",
+                logo: "./src/img/svg/udacity.svg",
+                issuer: "Udacity"
+            },
+            {
+                name: "Deep Learning Specialization",
+                link: "https://www.coursera.org/account/accomplishments/specialization/certificate/RF6JN7X9PNGR",
+                logo: "./src/img/svg/deeplearning-ai-icon.png",
+                issuer: "Deeplearning.ai"
+            },
+            {
+                name: "Machine Learning by Stanford University",
+                link: "https://www.coursera.org/account/accomplishments/certificate/GTHY9XTXZ75Q",
+                logo: "./src/img/svg/coursera.png",
+                issuer: "Coursera"
+            },
+            {
+                name: "Full Stack Web Development Nanodegree",
+                link: "https://graduation.udacity.com/confirm/6YYLUWEA",
+                logo: "./src/img/svg/udacity.svg",
+                issuer: "Udacity"
+            }
+
+        ]
+    }
+
     render() {
         return (<div className="card mt-3">
-            <div className="card-title card-header"><i className="fas fa-certificate"/> Certificates
+                <div className="card-title card-header"><i className="fas fa-certificate"/> Certificates
+                </div>
+                <table className="table table-bordered">
+                    <tbody>
+                    {
+                        this.state.certificates.map((certificate) => {
+                            return (
+                                <tr>
+                                    <td className="text-center">
+                                        <img src={certificate.logo} width="25" title={certificate.issuer}/>
+                                    </td>
+                                    <td>
+                                        <a href={certificate.link} target="_blank">{certificate.name}</a>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }
+                    </tbody>
+                </table>
             </div>
-            <table className="table table-bordered">
-                <tbody>
-                <tr>
-                    <td className="text-center"><img
-                        src="./src/img/svg/udacity.svg" width="25" title="Udacity" style={{webkitFilter: "grayscale(80%)"}}/></td>
-                    <td><a
-                        href="https://confirm.udacity.com/6YYLUWEA"> Cloud Developer for Enterprise Nanodegree</a></td>
-                </tr>
-                <tr>
-                    <td className="text-center"><img
-                        src="./src/img/svg/deeplearning-ai-icon.png" width="25" title="Deeplearning.ai"/></td>
-                    <td><a
-                        href="https://www.coursera.org/account/accomplishments/specialization/certificate/RF6JN7X9PNGR"> Deep
-                        Learning Specialization</a></td>
-                </tr>
-                <tr>
-                    <td className="text-center"><img
-                        src="./src/img/svg/coursera.png" width="25" title="Coursera"/>
-                    </td>
-                    <td><a
-                        href="https://www.coursera.org/account/accomplishments/certificate/GTHY9XTXZ75Q"> Machine
-                        Learning by
-                        Stanford
-                        University</a></td>
-                </tr>
-                <tr>
-                    <td className="text-center"><img src="./src/img/svg/udacity.svg" width="25" title="Udacity" style={{webkitFilter: "grayscale(80%)"}}/>
-                </td>
-                <td><a href="https://graduation.udacity.com/confirm/6YYLUWEA"> Full Stack Web
-                    Development
-                    Nanodegree</a></td>
-            </tr>
-        </tbody>
-    </table></div>
-    )
+        )
     }
 }
+
 ReactDOM.render(React.createElement(Certificates), document.querySelector('#certificates'));

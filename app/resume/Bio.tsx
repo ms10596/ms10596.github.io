f2c0eaa8987582096f0f92cf9602a50eedc7f5d1
Mohamed Sayed <ms10596@gmail.com>
2023-10-31 10:28:52 +0200

    Update Bio.tsx

--- app/resume/Bio.tsx
+++ app/resume/Bio.tsx
@@ -4,7 +4,7 @@ const Bio = () => (
   <Card>
     <div className="card-body">
       <p className="card-text">
-        Full-Stack Web Developer with a Passion for Type Safety Former data
+        Full-Stack Web Developer with a Passion for Type Safety. Former data
         scientist with a passion for building innovative and scalable web
         applications. Expertise in TypeScript, React, and Nest.js, with a focus
         on type safety between the front-end and back-end. Believes that type

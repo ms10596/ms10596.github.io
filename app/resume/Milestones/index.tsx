76900fd97a4f18c601676a4c8f5305ee3be61a03
ms10596 <ms10596@gmail.com>
2023-11-10 16:56:53 +0200

    primary color is black

--- app/resume/Milestones/index.tsx
+++ app/resume/Milestones/index.tsx
@@ -34,7 +34,9 @@ function Milestones() {
               imageFit: "contain",
             },
             mediaHeight: 100,
-            
+            theme: {
+              primary: "black",
+            },
           } as TimelineProps)}
         />
       </div>

f30acae73ba40bf122101333002f9b8227c6bce7
ms10596 <ms10596@gmail.com>
2023-11-11 15:53:58 +0200

    testing readdir

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,21 @@
+import Link from "next/link";
+import fs from "fs";
+import path from "path";
+
 export default function Page() {
-  return <h1>blog</h1>;
+  const routesPath = path.join(process.cwd(), "app", "blog");
+  const routesFiles = fs.readdirSync(routesPath);
+
+  return (
+    <div>
+      <h1>blog</h1>
+      {routesFiles
+        .filter((file) => file !== "page.tsx")
+        .map((file) => (
+          <Link key={file} href={`blog/${file}`}>
+            {file}
+          </Link>
+        ))}
+    </div>
+  );
 }

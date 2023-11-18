a6635bd86fa0e660643248f1ec05eae80327bf53
ms10596 <ms10596@gmail.com>
2023-11-18 13:54:56 +0200

    remote bio

--- app/resume/Bio.tsx
+++ app/resume/Bio.tsx
@@ -1,17 +1,16 @@
 import { Card, Em, Strong, Text } from "@radix-ui/themes";
 
-const Bio = () => (
-  <Card>
-    <Strong>
-      Full-Stack Web Developer with a strong interest for <Em>Type Safety.</Em>
-    </Strong>
-    <Text as="p">
-      Former data scientist with a passion for building innovative and scalable
-      web applications. Expertise in TypeScript, React, and Nest.js, with a
-      focus on type safety between the front-end and back-end. Believes that
-      type safety is crucial for delivering high-quality, bug-free code.
-    </Text>
-  </Card>
-);
+import { MDXRemote } from "next-mdx-remote/rsc";
+
+const Bio = async () => {
+  const res = await fetch(
+    "https://raw.githubusercontent.com/ms10596/ms10596/master/README.md"
+  ).then((res) => res.text());
+  return (
+    <Card>
+      <MDXRemote source={res} />
+    </Card>
+  );
+};
 
 export default Bio;

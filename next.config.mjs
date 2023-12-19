74dc59a123bac9525520fcf76cc5957e9e7c8560
ms10596 <ms10596@gmail.com>
2023-12-19 22:42:20 +0200

    Update Next.js configuration file

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,7 @@ const withMDX = createMDX({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: { unoptimized: true },
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 };
 export default withMDX(nextConfig);

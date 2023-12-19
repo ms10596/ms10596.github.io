287318da437b63904a985c8b4d30f8027f020bd4
ms10596 <ms10596@gmail.com>
2023-12-19 22:36:29 +0200

    unoptimized images

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,6 @@ const withMDX = createMDX({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: { unoptimized: true },
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 };
 export default withMDX(nextConfig);

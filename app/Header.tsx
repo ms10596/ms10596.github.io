47ab62258c09dfac512a796941c58cb55b89a344
ms10596 <ms10596@gmail.com>
2023-12-31 14:55:54 +0200

    avatar in header

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,18 +1,38 @@
+import { FaLaptopCode } from "react-icons/fa6";
+import Image from "next/image";
 import Link from "next/link";
 import { ThemeSwitcher } from "./components/theme-switcher";
 
 export function Header() {
   return (
     <div className="navbar print:hidden flex justify-between items-center">
-      <Link href={"/"} className="text-xl">
-        Mohamed Sayed
-      </Link>
+      <div>
+        <Link href={"/"} className="text-xl btn">
+          <Avatar />
+          Mohamed Sayed
+        </Link>
+      </div>
 
-      <div className="flex gap-x-9">
-        <Link href="/resume" className="flex-none">
-          Hire me
+      <div className="flex gap-x-9 print:hidden">
+        <Link href="/resume" className="flex-none btn">
+          <div>Hire me</div> <FaLaptopCode />
         </Link>
         <ThemeSwitcher />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <div className="avatar">
+      <div className="w-8 mask mask-circle">
+        <Image
+          src="https://avatars.githubusercontent.com/u/25807671?v=4"
+          alt="avatar"
+          width={32}
+          height={32}
+        />
       </div>
     </div>
   );
--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <div className="min-h-screen max-w-4xl">{children}</div>;
+  return <div className="min-h-screen max-w-7xl mx-auto py-10">{children}</div>;
 }

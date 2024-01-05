a20235b5c22dfe1ab43ad76340dee577c53bc384
ms10596 <ms10596@gmail.com>
2024-01-05 21:17:59 +0200

    fix: mobile view of resume

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -6,11 +6,11 @@ import Tools from "./Tools";
 export default function Page() {
   return (
     <div className="flex flex-col gap-y-3">
-      <div className="flex gap-x-9 items-stretch">
-        <div className="w-1/3 flex">
+      <div className="flex flex-col md:flex-row gap-x-9 gap-y-3 items-stretch">
+        <div className="w-full md:w-1/3 flex">
           <Intro />
         </div>
-        <div className="w-3/4 flex">
+        <div className="w-full md:w-3/4 flex">
           <Bio />
         </div>
       </div>

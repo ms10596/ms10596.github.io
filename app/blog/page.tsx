b5537a0e51eda1c43039a5076929f348fd80e99d
ms10596 <ms10596@gmail.com>
2024-01-07 15:59:52 +0200

    added a new blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -6,19 +6,28 @@ import path from "path";
 export default function Page() {
   const blogDir = path.join("content", "blogs");
 
-  const blogs = fs.readdirSync(path.join(blogDir)).map((filename) => {
-    const fileContent = fs.readFileSync(path.join(blogDir, filename), "utf-8");
-    const { data: frontMatter } = matter(fileContent);
-    return {
-      meta: frontMatter,
-      slug: filename.replace(".mdx", ""),
-    };
-  });
+  const blogs = fs
+    .readdirSync(path.join(blogDir))
+    .map((filename) => {
+      const fileContent = fs.readFileSync(
+        path.join(blogDir, filename),
+        "utf-8"
+      );
+      const { data: frontMatter } = matter(fileContent);
+      return {
+        meta: frontMatter,
+        slug: filename.replace(".mdx", ""),
+      };
+    })
+    .toSorted(
+      (a, b) =>
+        new Date(b.meta.date).getTime() - new Date(a.meta.date).getTime()
+    );
 
   return (
     <div className="pt-10">
       <h2 className="text-2xl font-bold">Latest Blogs</h2>
-      <ul className="menu w-full rounded-box bg-base-200">
+      <ul className="menu menu-lg w-full rounded-box bg-base-200">
         {blogs.map(({ slug, meta }) => (
           <li key={slug}>
             <Link
